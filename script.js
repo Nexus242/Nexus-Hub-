6304af577172765e5fb3c3b46be771d1e3badf4a
Nexus242 <wichainu896@gmail.com>
2025-03-27 18:51:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-// ฟังก์ชันดาวน์โหลดไฟล์
 function downloadFile() {
-    const url = "https://example.com/file.zip"; // ใส่ URL ของไฟล์ที่ต้องการดาวน์โหลด
-    const link = document.createElement('a');
-    link.href = url;
-    link.target = ''; 
-    link.rel = 'noopener noreferrer';
-    link.click();
+    alert("เร็วๆนี้...");
 }
-
-// ฟังก์ชันเปิด Discord
 function openDiscord() {
     window.open('https://discord.gg/kbeR5CHuFJ', '_blank'); 
 }
-
-// ฟังก์ชันเปิด YouTube
 function openYouTube() {
     window.open('https://www.youtube.com/@Zhinresvarant/videos', '_blank'); 
 }
-
+document.addEventListener("dragstart", function(event) {
+    event.preventDefault();
+});
+document.addEventListener("keydown", function(event) {
+    if (event.ctrlKey && (event.key === "c" || event.key === "u" || event.key === "s")) {
+        event.preventDefault();
+    }
+});
