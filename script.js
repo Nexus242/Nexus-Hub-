ebdf1eb3d04d360fe39ce68d158b33353f5094fe
Nexus242 <wichainu896@gmail.com>
2025-03-29 16:47:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function downloadFile() {
-    window.open('', '_blank'); 
+    window.open('https://filerift.com/file/jk2bmL9DIz', '_blank'); 
 }
 function openDiscord() {
     window.open('https://discord.gg/kbeR5CHuFJ', '_blank'); 
