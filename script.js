cd21f8587984f7b43930e22f85c95438f551bb78
Nexus242 <wichainu896@gmail.com>
2025-04-03 00:01:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function downloadFile() {
-    window.open('https://filerift.com/file/lCq5BQ1nAL', '_blank'); 
+    window.open('https://filerift.com/file/Pv8ND4IzBf', '_blank'); 
 }
 function openDiscord() {
     window.open('https://discord.gg/kbeR5CHuFJ', '_blank'); 
